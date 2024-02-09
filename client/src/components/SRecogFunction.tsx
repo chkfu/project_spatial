e717acac531047df1407718227745cd9d9d8e4f4
kchan <kwunfuchan@gmail.com>
2024-02-09 10:26:07 +0000

    [client] refine elements - control panel, SpeechRecog.tsx

---
+++ client/src/components/SRecogFunction.tsx
@@ -0,0 +1,36 @@
+import { updateMsgs } from "../redux/slices/recognizerSlice";
+
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+recognition.lang = 'zh-yue';
+recognition.interimResults = true;
+recognition.continuous = false;
+
+
+
+export function SRecogStarter(speaker: string, dispatch: any) {
+  // Recognizer Initialization
+  recognition.start();
+  console.log('[Recognisor] Default Speech Recognisor is activated...');
+  // Recognizer On-progress
+  recognition.onresult = (event: any) => {
+    console.log(event);
+    let temp: any[] = [];
+    if (event.results[0].isFinal) {
+      temp.push([speaker.toString(), event.results[0][0].transcript.toString()]);
+      dispatch(updateMsgs(temp));
+    }
+  };
+  // Recursion
+  recognition.onend = () => {
+    recognition.start();
+    console.log('[Recognizer] Default Speech Recognisor is activated again...');
+  };
+};
+
+export function SRecogTerminator() {
+  recognition.stop();
+  recognition.onresult = null;
+  recognition.onend = null;
+}
--- client/src/redux/slices/recognizerSlice.ts
+++ client/src/redux/slices/recognizerSlice.ts
@@ -3,16 +3,24 @@ import short from 'short-uuid';
 
 // Define a type for the slice state
 interface RecognizerState {
-  currentCaseId: string,
-  currentSpeaker: string,
+  // 1. main states - passive
+  currentCaseId: string;
+  currentSpeaker: string;
+  currentSection: number;
   newMsg: string[][];
+  // 2. additional status - buttons
+  recordingStatus: boolean;
+  recognizerStatus: string;
 }
 
 // Define the initial state using that type
 const initialState: RecognizerState = {
   currentCaseId: "default",
   currentSpeaker: "host",
-  newMsg: [["system", "#### Section 1 ####"]]
+  currentSection: 1,
+  newMsg: [["system", "#### Section 1 ####"]],
+  recordingStatus: false,
+  recognizerStatus: "inactive",
 };
 
 export const recognizerSlice = createSlice({
@@ -20,11 +28,15 @@ export const recognizerSlice = createSlice({
   // `createSlice` will infer the state type from the `initialState` argument
   initialState,
   reducers: {
+    // 1. Main - Passive 
     updateCaseId: (state, action: PayloadAction<string>) => {
       state.currentCaseId = action.payload;
     },
     switchSpeaker: (state, action: PayloadAction<string>) => {
       state.currentSpeaker = action.payload;
+    },
+    updateSectNum: (state) => {
+      state.currentSection += 1;
     },
     updateMsgs: (state, action: PayloadAction<string[][]>) => {
       if (state.newMsg[state.newMsg.length - 1][0] === state.currentSpeaker) {
@@ -34,13 +46,34 @@ export const recognizerSlice = createSlice({
         state.newMsg = [...state.newMsg, ...action.payload];
       }
     },
+    // 2. Additional - Control Panel
+    breakSection: (state, action: PayloadAction<string[]>) => {
+      state.newMsg = [...state.newMsg, action.payload];
+    },
+    setRecording: (state) => {
+      if (state.recordingStatus === true)
+        state.recordingStatus = false;
+      else if (state.recordingStatus === false)
+        state.recordingStatus = true;
+    },
+    setRecognizer: (state) => {
+      if (state.recognizerStatus === "inactive")
+        state.recognizerStatus = "started";
+      else if (state.recognizerStatus === "started" || state.recognizerStatus === "resumed")
+        state.recognizerStatus = "stopped";
+      else if (state.recognizerStatus === "stopped")
+        state.recognizerStatus = "resumed";
+    },
     resetRecog: (state) => {
-
+      state.newMsg = [["system", "#### Section 1 ####"]];
+      state.recognizerStatus = "inactive";
+      state.recordingStatus = false;
+      state.currentSection = 1;
     },
   },
 });
 
-export const { switchSpeaker, updateMsgs, resetRecog } = recognizerSlice.actions;
+export const { switchSpeaker, updateMsgs, resetRecog, setRecognizer, setRecording, breakSection, updateSectNum } = recognizerSlice.actions;
 
 // Other code such as selectors can use the imported `RootState` type
 export default recognizerSlice.reducer;
